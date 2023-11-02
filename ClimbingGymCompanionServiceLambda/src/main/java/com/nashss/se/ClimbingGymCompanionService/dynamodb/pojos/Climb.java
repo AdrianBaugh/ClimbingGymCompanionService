306277e9012ec1082090903c8570e1db6d3b4565
AdrianBaugh <adrian.baugh@gmail.com>
2023-11-02 16:01:59 -0500

    created route and climb pojos, localDate and time converters

--- ClimbingGymCompanionServiceLambda/src/main/java/com/nashss/se/ClimbingGymCompanionService/dynamodb/pojos/Climb.java
+++ ClimbingGymCompanionServiceLambda/src/main/java/com/nashss/se/ClimbingGymCompanionService/dynamodb/pojos/Climb.java
@@ -1,4 +1,108 @@
 package com.nashss.se.ClimbingGymCompanionService.dynamodb.pojos;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverted;
+import com.nashss.se.ClimbingGymCompanionService.converters.LocalDateTimeConverter;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+/**
+ * Represents a climb in the climbs table.
+ */
+@DynamoDBTable(tableName= "climbs" )
 public class Climb {
+    private String climbId;
+    private String userId;
+    private String routeId;
+    private String climbStatus;
+    private LocalDateTime dateTimeClimbed;
+    private Boolean thumbsUp;
+    private String notes;
+    @DynamoDBRangeKey(attributeName = "climbId")
+    public String getClimbId() {
+        return climbId;
+    }
+
+    public void setClimbId(String climbId) {
+        this.climbId = climbId;
+    }
+    @DynamoDBHashKey(attributeName = "userId")
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    @DynamoDBAttribute(attributeName = "routeId")
+    public String getRouteId() {
+        return routeId;
+    }
+
+    public void setRouteId(String routeId) {
+        this.routeId = routeId;
+    }
+    @DynamoDBAttribute(attributeName = "climbStatus")
+    public String getClimbStatus() {
+        return climbStatus;
+    }
+
+    public void setClimbStatus(String climbStatus) {
+        this.climbStatus = climbStatus;
+    }
+    @DynamoDBAttribute(attributeName = "dateTimeClimbed")
+    @DynamoDBTypeConverted(converter = LocalDateTimeConverter.class)
+    public LocalDateTime getDateTimeClimbed() {
+        return dateTimeClimbed;
+    }
+
+    public void setDateTimeClimbed(LocalDateTime dateTimeClimbed) {
+        this.dateTimeClimbed = dateTimeClimbed;
+    }
+    @DynamoDBAttribute(attributeName = "thumbsUp")
+    public Boolean getThumbsUp() {
+        return thumbsUp;
+    }
+
+    public void setThumbsUp(Boolean thumbsUp) {
+        this.thumbsUp = thumbsUp;
+    }
+    @DynamoDBAttribute(attributeName = "notes")
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+        Climb climb = (Climb) other;
+        return Objects.equals(climbId, climb.climbId) && Objects.equals(userId, climb.userId) && Objects.equals(routeId, climb.routeId) && Objects.equals(climbStatus, climb.climbStatus) && Objects.equals(dateTimeClimbed, climb.dateTimeClimbed) && Objects.equals(thumbsUp, climb.thumbsUp) && Objects.equals(notes, climb.notes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(climbId, userId, routeId, climbStatus, dateTimeClimbed, thumbsUp, notes);
+    }
+
+    @Override
+    public String toString() {
+        return "Climb{" +
+                "climbId='" + climbId + '\'' +
+                ", userId='" + userId + '\'' +
+                ", routeId='" + routeId + '\'' +
+                ", climbStatus='" + climbStatus + '\'' +
+                ", dateTimeClimbed=" + dateTimeClimbed +
+                ", thumbsUp=" + thumbsUp +
+                ", notes='" + notes + '\'' +
+                '}';
+    }
 }
--- ClimbingGymCompanionServiceLambda/src/main/java/com/nashss/se/ClimbingGymCompanionService/dynamodb/pojos/Route.java
+++ ClimbingGymCompanionServiceLambda/src/main/java/com/nashss/se/ClimbingGymCompanionService/dynamodb/pojos/Route.java
@@ -1,4 +1,132 @@
 package com.nashss.se.ClimbingGymCompanionService.dynamodb.pojos;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.*;
+import com.nashss.se.ClimbingGymCompanionService.converters.LocalDateConverter;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@DynamoDBTable(tableName = "routes")
 public class Route {
+    private String routeId;
+    private String routeStatus;
+    private String location;
+    private String color;
+    private String type;
+    private String difficulty;
+    private LocalDate dateCreated;
+    private Integer rating;
+    private String pictureKey;
+    private static final String GSI_INDEX_NAME = "RoutesByStatusIndex";
+
+    @DynamoDBHashKey(attributeName = "routeId")
+    public String getRouteId() {
+        return routeId;
+    }
+
+    public void setRouteId(String routeId) {
+        this.routeId = routeId;
+    }
+
+    @DynamoDBAttribute(attributeName = "routeStatus")
+    @DynamoDBIndexHashKey(globalSecondaryIndexName = GSI_INDEX_NAME)
+    public String getRouteStatus() {
+        return routeStatus;
+    }
+
+    public void setRouteStatus(String routeStatus) {
+        this.routeStatus = routeStatus;
+    }
+
+    @DynamoDBAttribute(attributeName = "location")
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @DynamoDBAttribute(attributeName = "color")
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @DynamoDBAttribute(attributeName = "type")
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @DynamoDBAttribute(attributeName = "difficulty")
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    @DynamoDBAttribute(attributeName = "dateCreated")
+    @DynamoDBTypeConverted(converter = LocalDateConverter.class)
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDate dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    @DynamoDBAttribute(attributeName = "rating")
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    @DynamoDBAttribute(attributeName = "pictureKey")
+    public String getPictureKey() {
+        return pictureKey;
+    }
+
+    public void setPictureKey(String pictureKey) {
+        this.pictureKey = pictureKey;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+        Route route = (Route) other;
+        return Objects.equals(routeId, route.routeId) && Objects.equals(routeStatus, route.routeStatus) && Objects.equals(location, route.location) && Objects.equals(color, route.color) && Objects.equals(type, route.type) && Objects.equals(difficulty, route.difficulty) && Objects.equals(dateCreated, route.dateCreated) && Objects.equals(rating, route.rating) && Objects.equals(pictureKey, route.pictureKey);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(routeId, routeStatus, location, color, type, difficulty, dateCreated, rating, pictureKey);
+    }
+
+    @Override
+    public String toString() {
+        return "Route{" +
+                "routeId='" + routeId + '\'' +
+                ", routeStatus='" + routeStatus + '\'' +
+                ", location='" + location + '\'' +
+                ", color='" + color + '\'' +
+                ", type='" + type + '\'' +
+                ", difficulty='" + difficulty + '\'' +
+                ", dateCreated=" + dateCreated +
+                ", rating=" + rating +
+                ", pictureKey='" + pictureKey + '\'' +
+                '}';
+    }
 }
