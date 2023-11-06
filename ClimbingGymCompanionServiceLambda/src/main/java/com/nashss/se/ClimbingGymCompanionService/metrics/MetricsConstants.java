4936e7612eb6001470b026be395a2429b19d4846
AdrianBaugh <adrian.baugh@gmail.com>
2023-11-06 14:35:47 -0600

    updated metrics class and updated yaml

--- ClimbingGymCompanionServiceLambda/src/main/java/com/nashss/se/ClimbingGymCompanionService/metrics/MetricsConstants.java
+++ ClimbingGymCompanionServiceLambda/src/main/java/com/nashss/se/ClimbingGymCompanionService/metrics/MetricsConstants.java
@@ -4,11 +4,18 @@ package com.nashss.se.ClimbingGymCompanionService.metrics;
  * Constant values for use with metrics.
  */
 public class MetricsConstants {
-//    public static final String GETPLAYLIST_PLAYLISTNOTFOUND_COUNT = "GetPlaylist.PlaylistNotFoundException.Count";
-//    public static final String UPDATEPLAYLIST_INVALIDATTRIBUTEVALUE_COUNT =
-//        "UpdatePlaylist.InvalidAttributeValueException.Count";
-//    public static final String UPDATEPLAYLIST_INVALIDATTRIBUTECHANGE_COUNT =
-//        "UpdatePlaylist.InvalidAttributeChangeException.Count";
+    public static final String GETCLIMB_PLAYLISTNOTFOUND_COUNT =
+            "GetClimb.ClimbNotFoundException.Count";
+    public static final String GETROUTE_ROUTENOTFOUND_COUNT =
+            "GetRoute.RouteNotFoundException.Count";
+    public static final String UPDATECLIMB_INVALIDATTRIBUTEVALUE_COUNT =
+        "UpdateClimb.InvalidAttributeValueException.Count";
+    public static final String UPDATEROUTE_INVALIDATTRIBUTECHANGE_COUNT =
+        "UpdateRoute.InvalidAttributeChangeException.Count";
+    public static final String CREATECLIMB_INVALIDATTRIBUTEVALUE_COUNT =
+            "CreateClimb.InvalidAttributeValueException.Count";
+    public static final String CREATEROUTE_INVALIDATTRIBUTECHANGE_COUNT =
+            "CreateRoute.InvalidAttributeChangeException.Count";
     public static final String SERVICE = "Service";
     public static final String SERVICE_NAME = "ClimbingGymCompanionService";
     public static final String NAMESPACE_NAME = "Capstone/ClimbingGymCompanionService";
