daa0f518e5e3d338bdb6b803990efbb4cc35167f
AdrianBaugh <adrian.baugh@gmail.com>
2023-11-06 16:11:23 -0600

    front end setup of basic frontend classes

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -1,4 +1,4 @@
-import MusicPlaylistClient from '../api/musicPlaylistClient';
+import ClimbClient from '../api/climbClient';
 import BindingClass from "../util/bindingClass";
 
 /**
@@ -14,7 +14,7 @@ export default class Header extends BindingClass {
         ];
         this.bindClassMethods(methodsToBind, this);
 
-        this.client = new MusicPlaylistClient();
+        this.client = new ClimbClient();
     }
 
     /**
@@ -35,7 +35,7 @@ export default class Header extends BindingClass {
         const homeButton = document.createElement('a');
         homeButton.classList.add('header_home');
         homeButton.href = 'index.html';
-        homeButton.innerText = 'Playlists';
+        // homeButton.innerText = 'Playlists';
 
         const siteTitle = document.createElement('div');
         siteTitle.classList.add('site-title');
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -23,9 +23,11 @@ module.exports = {
     usedExports: true
   },
   entry: {
-    createPlaylist: path.resolve(__dirname, 'src', 'pages', 'createPlaylist.js'),
-    viewPlaylist: path.resolve(__dirname, 'src', 'pages', 'viewPlaylist.js'),
-    searchPlaylists: path.resolve(__dirname, 'src', 'pages', 'searchPlaylists.js'),
+    // createPlaylist: path.resolve(__dirname, 'src', 'pages', 'createPlaylist.js'),
+    // viewPlaylist: path.resolve(__dirname, 'src', 'pages', 'viewPlaylist.js'),
+    // searchPlaylists: path.resolve(__dirname, 'src', 'pages', 'searchPlaylists.js'),
+    home: path.resolve(__dirname, 'src', 'pages', 'index.js'),
+
   },
   output: {
     path: path.resolve(__dirname, 'build', 'assets'),
